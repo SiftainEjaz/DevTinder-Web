00da635c3bf5806bc4335c8bd31f44bb83c6f5e4
Siftain Ejaz <siftainejaz@gmail.com>
2025-07-22 12:31:36 +0530

    Added basic navbar, footer. Implemented basic react routers.

---
+++ src/Body.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import NavBar from './NavBar.jsx';
+import { Outlet } from 'react-router-dom';
+import Footer from './Footer.jsx';
+
+const Body = () => {
+
+    return (
+        <div>
+            <NavBar/>
+            {/* Any children routes of the body will render here */}
+            <Outlet/> 
+            <Footer />   
+        </div>
+    )
+}
+
+export default Body;
+
